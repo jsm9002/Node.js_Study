8527a90134ced9f3fc51759ebe44df57f6c4cc8b
jsm9002 <jsm9002@naver.com>
2023-07-31 13:58:35 +0900

    "DB에서 받은 데이터를 HTML 로 보내기 23/07/31"

--- 0727Express/app.js
+++ 0727Express/app.js
@@ -15,6 +15,19 @@ const app = express();
 const router = require("./router/router")
 
 
+
+//동적페이지를 사용할 수 있는 nunjucks 가져오기
+const nunjucks = require("nunjucks")
+//view engine 확장자를 html로 사용하겠다!
+// html 파일들을 동적파일로 사용할 수 있게 만들겠다!
+app.set("view engine","html")
+//views 안에 있는 html을 동적파일로 사용할 수 있게끔 만들겠다!!
+//동적파일 --> 데이터가 바뀔때마다 화면 내용도 같이 바뀌는 파일
+nunjucks.configure("views",{
+    express: app,
+    watch:true
+})
+
 //4. 경로를 설정할 수 있는 Router 만들기
 // const router = express.Router();
 // router.get("/",function(request,response){
@@ -26,6 +39,8 @@ const router = require("./router/router")
 //     console.log(request.query.text)
 //     response.end()
 // })
+
+
 //5. router로 만든 경로를 서버(app)에 등록 시켜주기!
 // 서버에 등록시킨다 ==> 미들웨어
 // body영역 허용뒤 -> router 등록! 
